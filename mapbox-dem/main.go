57252158ef06f5bf97a4aad07904089099b44770
Doraemon <40662940+dongliangde@users.noreply.github.com>
2023-11-07 10:17:03 +0800

    Update main.go

--- mapbox-dem/main.go
+++ mapbox-dem/main.go
@@ -15,26 +15,26 @@ func main() {
 	if err != nil {
 		log.Println(err)
 	}
-	//珠峰经纬度
+	// 珠峰经纬度
 	//lng := 86.92498
 	//lat := 27.98812
-	//香炉峰经纬度
+	// 香炉峰经纬度
 	//lng := 116.1785
 	//lat := 39.99215
 	lng := 85.77917
 	lat := 28.3525
-	//图层
+	// 图层
 	zoom := 14
-	//获取瓦片地址
+	// 获取瓦片地址
 	x := ((lng + 180.0) / 360.0) * math.Exp2(float64(zoom))
 	y := (1 - math.Asinh(math.Tan(lat*math.Pi/180))/math.Pi) * math.Exp2(float64(zoom-1))
-	//可以直接访问mapbox官网获取webp数据
-	//计算rgba坐标
+	// 可以直接访问mapbox官网获取webp数据
+	// 计算rgba坐标
 	pixelIntX := int(math.Floor(math.Mod(x*514, 514)))
 	pixelIntY := int(math.Floor(math.Mod(y*514, 514)))
-	//解析图片
+	// 解析图片
 	img, err := webp.Decode(bytes.NewBuffer(data))
-	//读取图片RGBA值
+	// 读取图片RGBA值
 	colorRGB := img.(image.Image).At(pixelIntX, pixelIntY)
 	r, g, b, _ := colorRGB.RGBA()
 	height := -10000 + float64(int(uint8(r))*256*256+int(uint8(g))*256+int(uint8(b)))*0.1
